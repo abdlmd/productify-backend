e947177a195938c66624465794111680bc91b38a
abdlmd <abdulmueedhabibraja@gmail.com>
2025-03-30 00:24:23 +0500

    initial comit

---
+++ src/test/java/com/example/productify_backend/ProductifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
